617041b7af2081a2f9ea2eb02e732d14fb50d721
Panuwit Supornpipat <panuwit@emcsthai.com>
2022-01-04 17:49:45 +0700

    update

--- src/VuePictureSwipe.vue
+++ src/VuePictureSwipe.vue
@@ -11,12 +11,14 @@
         itemtype="http://schema.org/ImageObject"
         :src="item.src"
       >
-        <figcaption
-          itemprop="caption description"
-          v-if="item.figcaption"
-          :template="item.figcaption"
-          @click="item.functionstring"
-        ></figcaption>
+        <figcaption itemprop="caption description" v-if="item.functionstring">
+          <button
+            type="button"
+            class="btn-close"
+            aria-label="Close"
+            @click="item.functionstring"
+          ></button>
+        </figcaption>
         <a
           v-show="nbThumbnailsDisplayed === -1 || index < nbThumbnailsDisplayed"
           :href="item.src"
